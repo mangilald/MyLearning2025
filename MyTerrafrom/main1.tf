a6596e6de17d921af116f57102d342c54c148a54
mangilald <mangidaravath@gmail.com>
2025-03-16 12:55:20 +0530

    up

---
+++ MyTerrafrom/main1.tf
@@ -0,0 +1,17 @@
+terraform i
+required_providers {
+aws = {
+source = "hashicorp/aws"
+version = "5.74.0"
+}
+provider "aws" {
+region = "us-east-1"
+}
+resource "aws_s3_bucket" "example" {
+bucket = "my-tf-test-bucketxxxaxaxaxaxasasassdadasd"
+tags = 1
+Name
+= "My bucket"
+Environment = "Stage"
+}
+}
